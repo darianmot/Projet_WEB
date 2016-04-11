7cd7c9db659223331a490279272aec1ca5d1fec8
Darian Motamed <darian.motamed@gmail.com>
2016-04-11 10:31:55 +0200

    Add php skeleton.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Mon super site</title>
+</head>
+
+<body>
+
+<?php include("template/header.php"); ?>
+
+<?php include("template/menu.php"); ?>
+
+<!-- Le corps -->
+
+<div>
+    <h1>Mon super site</h1>
+    
+</div>
+
+<!-- Le pied de page -->
+
+<?php include("template/footer.php"); ?>
+
+</body>
+</html>
