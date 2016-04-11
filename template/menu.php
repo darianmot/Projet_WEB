7e601f5ebe030c9282ba7308181d7771953aa0ce
Atime Ronda <atime.ronda@gmail.com>
2016-04-11 11:44:04 +0200

    Style et image voiture  bouton

--- template/menu.php
+++ template/menu.php
@@ -1,1 +1,12 @@
+<nav>
+
+    <div id='cssmenu'>
+        <ul>
+            <li class='active'><a href='#'>Accueil</a></li>
+            <li><a href='#'>Connexion</a></li>
+
+        </ul>
+    </div>
 <?php echo "menu section" ?>
+
+</nav>
