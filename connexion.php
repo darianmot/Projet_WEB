86fc9ef434334c7b374c7e45333072367d8a4edf
Anas Darwich <anas.darwich @aliceadsl.fr>
2016-04-13 15:23:11 +0200

    #début de la page connexion

--- connexion.php
+++ connexion.php
@@ -4,13 +4,13 @@
 <body>
 <?php include("template/header.php"); ?>
 <?php include("template/menu.php") ?>
-
-<a href="index.php"><input type="button" value="Précédent"></a>
-
-
-
-
-
+<table><thead><th>Identifiant</th></thead>
+    <thead><th>Mot de passe</th></thead>
+    <tfoot>
+    
+    </tfoot>
+</table>
+<a href="index.php"><input type="button" value="Précédent"><f</a>
 
 </body>
 </html>
