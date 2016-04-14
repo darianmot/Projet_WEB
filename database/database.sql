f73b44e28b58eb2d449670d7aa11397355e10bea
Darian <darian.motamed@gmail.com>
2016-04-14 17:21:51 +0200

    Update database

--- database/database.sql
+++ database/database.sql
@@ -6,16 +6,18 @@ USE ienac15_;
 
 CREATE TABLE Zone
 (
-  id_zone INT NOT NULL auto_increment,
+  id_zone INT NOT NULL,
   nom_zone CHAR(20),
+  capacite INT,
   PRIMARY KEY (id_zone)
 )ENGINE=INNODB;
 
 
 CREATE TABLE Emplacement
 (
-  id_emplacement INT NOT NULL auto_increment,
+  id_emplacement INT NOT NULL,
   id_zone INT,
+  occupe CHAR(20) DEFAULT NULL,
   PRIMARY KEY (id_emplacement),
   FOREIGN KEY (id_zone) REFERENCES Zone(id_zone)
 )ENGINE=INNODB;
@@ -25,9 +27,32 @@ CREATE TABLE Client
 (
   plaque CHAR(20) NOT NULL,
   type_vehicule CHAR(20),
-  emplacement_choisi INT,
-  emplacement_attribue INT,
+  date_entree DATETIME,
+  reservation CHAR(20),
+  zone_choisi INT,
   PRIMARY KEY (plaque),
-  FOREIGN KEY (emplacement_choisi) REFERENCES  Emplacement(id_emplacement),
-  FOREIGN KEY (emplacement_attribue) REFERENCES Emplacement(id_emplacement)
+  FOREIGN KEY (zone_choisi) REFERENCES Zone(id_zone)
 )ENGINE=INNODB;
+
+CREATE TABLE PlaceOccupee
+(
+  id_client CHAR(20) NOT NULL,
+  id_attribution INT NOT NULL,
+  id_choisi INT NOT NULL,
+  PRIMARY KEY (id_client,id_attribution),
+  FOREIGN KEY (id_client) REFERENCES Client(plaque),
+  FOREIGN KEY (id_attribution) REFERENCES Emplacement(id_emplacement),
+  FOREIGN KEY (id_choisi) REFERENCES Emplacement(id_emplacement)
+)ENGINE=INNODB;
+
+CREATE TABLE Facture
+(
+  id_facture CHAR(20) NOT NULL,
+  prix_tot INT,
+  date DATETIME,
+  penalite INT,
+  plaque CHAR(20),
+  PRIMARY KEY (id_facture),
+  FOREIGN KEY (plaque) REFERENCES Client(plaque)
+)ENGINE=INNODB;
+
