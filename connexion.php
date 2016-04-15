7b6a4337daa086dc9f582936c6079681d374b62f
Anas Darwich <anas.darwich @aliceadsl.fr>
2016-04-16 00:50:08 +0200

    #insertion des formulaires dans la page de connexion

--- connexion.php
+++ connexion.php
@@ -3,14 +3,14 @@
 <?php include("template/head.php") ?>
 <body>
 <?php include("template/header.php"); ?>
-<?php include("template/menu.php") ?>
-<table><thead><th>Identifiant</th></thead>
-    <thead><th>Mot de passe</th></thead>
-    <tfoot>
-    
-    </tfoot>
+<!--?php include("template/menu.php") ?-->
+<h1>Connexion</h1>
+<table><tr><th>\</th><th>Champs  obligatoires</th></tr>
+    <tbody>
+    <tr><td>Identifiant</td><td><form><input type="text" name="texte" value="..."></form></td></tr>
+    <tr><td>Password</td><td><form><input type="text" name="texte" value="..."></form></td></tr>
+    </tbody>
 </table>
 <a href="index.php"><input type="button" value="Précédent"><f</a>
-
 </body>
 </html>
