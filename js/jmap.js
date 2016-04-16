e52ff176ea9f50b170e9d5db0cdc9e687e6b0520
Simon <simon.aldouri.ravoux@gmail.com>
2016-04-16 23:15:05 +0200

    Carte

---
+++ js/jmap.js
@@ -0,0 +1,47 @@
+var map;
+var initialize;
+
+initialize = function(){
+  var latLng = new google.maps.LatLng(50.6371834, 3.063017400000035); // Correspond au coordonnées de Lille
+  var myOptions = {
+    zoom      : 14, // Zoom par défaut
+    center    : latLng, // Coordonnées de départ de la carte de type latLng 
+    mapTypeId : google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
+    maxZoom   : 20
+  };
+  
+  map      = new google.maps.Map(document.getElementById('map'), myOptions);
+  
+  var marker = new google.maps.Marker({
+    position : latLng,
+    map      : map,
+    title    : "Parking de l'aéroport"
+    //icon     : "marker_lille.gif" // Chemin de l'image du marqueur pour surcharger celui par défaut
+  });
+  
+  var contentMarker = [
+      '<div id="containerTabs">',
+      '<div id="tabs">',
+      '<div id="tab-1">',
+        '<h3>Parking</h3><p>Le parking à votre disposition avant le départ en voyage</p>',
+      '</div>',
+      '</div>',
+      '</div>',
+      '</div>'
+  ].join('');
+
+  var infoWindow = new google.maps.InfoWindow({
+    content  : contentMarker,
+    position : latLng
+  });
+  
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.open(map,marker);
+  });
+  
+  google.maps.event.addListener(infoWindow, 'domready', function(){ // infoWindow est biensûr notre info-bulle
+    jQuery("#tabs").tabs();
+  });
+};
+
+initialize();
---
+++ template/carte.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Google Maps Api v3</title>
+</head>
+<style type="text/css">
+    #container{position:relative;width:990px;margin:auto;}
+    #container #map{width:500px;height:500px;margin:auto;}
+</style>
+<body>
+<div id="container">
+    <div id="map">
+        <p>Veuillez patienter pendant le chargement de la carte...</p>
+    </div>
+</div>
+
+<!-- Include Javascript -->
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="js/jmap.js"></script>
+</body>
+</html>
