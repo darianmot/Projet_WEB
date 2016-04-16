ffaac08b66c06f8ce9696c0446fbe61abbd03734
Darian <darian.motamed@gmail.com>
2016-04-16 21:00:05 +0200

    Suppresion de la bordure du slider

--- template/slider.php
+++ template/slider.php
@@ -1,6 +1,6 @@
 <!-- Start WOWSlider.com BODY section -->
 <div id="wowslider-container1">
-	<div class="ws_images"><ul>
+	<div class="ws_images" style="border:none;"><ul>
 			<li><img src="media/images/accueil_park.jpg" alt="Accueil" title="abstarct_bluetexture" id="wows1_0"/></li>
 			<li><img src="media//images/planepictureshd.jpg" alt="wow slider" title="Plane-Pictures-HD" id="wows1_1"/></li>
 			<li><img src="media/images/osakaairportnightbuswallpaper.jpg" alt="osaka-airport-night-bus-wallpaper" title="osaka-airport-night-bus-wallpaper" id="wows1_2"/></li>
