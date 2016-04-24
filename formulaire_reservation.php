9f70ce98ca366d1c82ea5bee014a3fed3deeba55
Atime Ronda <atime.ronda@gmail.com>
2016-04-24 19:37:16 +0200

    Formulaire de réservation, ajout de bootstrap, proposition de background plus discret

--- formulaire_reservation.php
+++ formulaire_reservation.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
 <?php include("template/head.php"); ?>
 
-<html>
-<div class="reserv">
-    <h3>Réservez votre place ici !</h3>
-    <input class="encart_reserv" type="text" name="nom" placeholder="Nom">
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<form method="post" action="" enctype="multipart/form-data">
 
-</div>
+    <div class="container reserv">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Réservez votre place ici !</h3>
+            </div>
+        </div>
+
+        </br>
+
+        <div class="row">
+            <div class="col-md-3">
+                <input class="encart_reserv" type="text" name="nom" placeholder="Nom"/>
+            </div>
+
+            <div class="col-md-3">
+                <input class="encart_reserv" type="text" name="prenom" placeholder="Prenom"/>
+            </div>
+
+            <div class="col-md-3">
+                <input class="encart_reserv" type="text" name="immatriculation" placeholder="N° d'immatriculation"/>
+            </div>
+        </div>
+
+        </br>
+
+        <div class="row">
+            <div class="col-md-3">
+                <select name="type" size="1">
+                    <option>Voiture</option>
+                    <option>Moto</option>
+                </select>
+            </div>
+
+            <div class="col-md-3">
+                <input type="checkbox" name="handicap" value="1">Véhicule handicapé ?
+            </div>
+
+            <div>
+                <button type="submit" name="button_reserv">Réserver maintenant !</button>
+            </div>
+
+        </div>
+
+    </div>
 
 
+
+</form>
 </html>
--- template/head.php
+++ template/head.php
@@ -1,11 +1,13 @@
 <head>
     <meta charset="utf-8" />
     <title>Car Park</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/zone_view.css">
     <link rel="stylesheet" type="text/css" href="css/banniere.css">
+
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/menu_reservation.js"></script>
-    <script type="text/javascript" src="js/menu2.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
 </head>
