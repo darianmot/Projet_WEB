e720138b170a63b7b2638a7cba68fadb805a3c3c
Atime Ronda <atime.ronda@gmail.com>
2016-04-28 01:03:39 +0200

    Quelques ajustements sur l'estimateur de prix, vérification de la sélection de zone, arrondi au centime (personne ne voudrait payer 12.399995478 € :p)

--- js/price.js
+++ js/price.js
@@ -5,19 +5,25 @@
 $(document).ready(function () {
     $("#button_price").click(function () {
 
-        var price_of_day = 20;
-        var price_of_hour = 6;
+
+        var price_of_day = 15;
+        var price_of_hour = 2;
         var price_of_minute = 0.2;
         var coef_zone = ($('input[name="zone_price"]:checked').val() /10) + 0.9;
         var number_day = $('input[name="duree_jours"]').val();
         var number_hours = $('input[name="duree_hours"]').val();
         var number_min = $('input[name="duree_min"]').val();
 
-        var price = coef_zone * (number_day*price_of_day + number_hours *  price_of_hour + number_min *  price_of_minute);
+        if (isNaN(coef_zone)  ) {
+            alert("Veuillez sélectionner une zone puis relancer l'estimation")
+        }
 
-        $('#disp_price').empty();
-        $('#disp_price').prepend('<h2> Total TTC: </h2>'+ '<h4>' +  price + '€ '+ '</h4>');
-
+        else {
+            var price = coef_zone * (number_day*price_of_day + number_hours *  price_of_hour + number_min *  price_of_minute);
+            price = price.toFixed(2);
+            $('#disp_price').empty();
+            $('#disp_price').prepend('<h2> Total TTC: </h2>'+ '<h4>' +  price + '€ '+ '</h4>');
+        }
 
 
     })
