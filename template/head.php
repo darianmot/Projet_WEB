e301eb23e836d004e05525fd73d21327842eba4a
Atime Ronda <atime.ronda@gmail.com>
2016-04-29 01:18:21 +0200

    Ajout d'une petite favicon (l'image que vous voyez dans l'onglet de votre navigateur à coté du titre du site), ça fait toujours plaisir

--- template/head.php
+++ template/head.php
@@ -2,12 +2,13 @@
     <meta charset="utf-8" />
     <title>Car Park</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/parking_view.css">
-    <link rel="stylesheet" type="text/css" href="css/banniere.css">
-    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.min.css">
-    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.structure.min.css">
-    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/parking_view.css"/>
+    <link rel="stylesheet" type="text/css" href="css/banniere.css"/>
+    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.structure.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/ui_css/jquery-ui.theme.min.css"/>
+    <link rel="icon" type="image/png" href="media/images/favicon_proposal.png" />
 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery-ui.min.js"></script>
