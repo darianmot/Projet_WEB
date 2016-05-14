00034b94bc87bc8faac6ea0980c108edc54a2e9a
anas darwich <anas.darwich@aliceadsl.fr>
2016-05-14 02:06:40 +0200

    #petits problème de mis ç jour de table

--- database/bdd_connection.php
+++ database/bdd_connection.php
@@ -7,7 +7,7 @@ class Connection
     /*Construteur*/
     public function __construct()
     {
-        $this->setBdd(new mysqli('localhost', 'root', 'mysql', 'database.sql'));
+        $this->setBdd(new mysqli('localhost', 'root', 'mysql', 'ienac15_'));
     }
 
     /*Getter*/
