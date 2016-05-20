96ba39381890736baea83dce421c109b0b37fbc2
Anas Darwich <anas.darwich @aliceadsl.fr>
2016-05-20 23:25:38 +0200

    #nettoyage du code connexion, inscription

--- js/connexion.js
+++ js/connexion.js
@@ -3,21 +3,27 @@ $(document).ready(function () {
         e.preventDefault();
         //alert($('#connexion_form').serialize());
         $.ajax({
+            
             type: "POST",
             url: " database/connexion.php",
             dataType : 'html',
             data: $('#connexion_form').serialize(),
             success: function(msg)
-            { //alert(msg);
-            if (msg !='echec') {
-                $('#menu_p').append(msg);
-                $('#bloc_connexion').remove();
-                $('#connexion').text('Vous etes connecté');
-            }
-            else
-            {alert('identifiant ou mot de passe incorrectes');
-                $('#identifiant').val('');
-                $('#password').val('');}
+            {
+                if (msg !='echec') 
+                {
+                    $('#menu_p').append(msg);
+                    $('#bloc_connexion').remove();
+                    $('#connexion').text('Vous etes connecté');
+                }
+            
+                else
+            
+                {
+                    alert('identifiant ou mot de passe incorrectes');
+                    $('#identifiant').val('');
+                    $('#password').val('');
+                }
                 $('#onglet_connexion').text('');
             },
             error: function(retour)
