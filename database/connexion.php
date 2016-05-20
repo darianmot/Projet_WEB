f1adada93905555c8d5dc6b830d6f7508666073b
Anas Darwich <anas.darwich @aliceadsl.fr>
2016-05-20 23:27:25 +0200

    #nettoyage du code connexion, inscription

--- database/connexion.php
+++ database/connexion.php
@@ -1,22 +1,21 @@
 <?php
-// demarrage de la session
 include "bdd_connection.php";
-session_start();
 // creation des variables de session
+session_start();
+
 $connection = new Connection();
 $bdd = $connection->getBdd();
-//donnee brutes
+
 $reponse = $bdd->query("SELECT id_utilisateur,password  FROM Utilisateur WHERE id_utilisateur='{$_POST['identifiant']}'");
-//premiere ligne
 $donnees=$reponse->fetch_assoc();
-//verification des donnees
+
 
 if (isset($_POST['password'])AND isset($_POST['identifiant']))
 {
     if (($donnees['password'])==$_POST['password'])
-    {$_SESSION['identifiant']=$_POST['identifiant'];
+    {
+        $_SESSION['identifiant']=$_POST['identifiant'];
         $_SESSION['password']=$_POST['password'];
-
         echo(" <li id=offres> Bienvenue {$_SESSION['identifiant']}
                 <ul id='menu2'>
     
@@ -33,5 +32,7 @@ if (isset($_POST['password'])AND isset($_POST['identifiant']))
     }
 }
 else
-{echo('vide');}
+{
+    echo('vide');
+}
 ?>
