5e7a734d87cf1abca4bc4b95cea59415264797d3
Simon <simon.aldouri.ravoux@gmail.com>
2016-05-24 22:02:19 +0200

    Slider et menu

--- template/slider.php
+++ template/slider.php
@@ -1,8 +1,9 @@
 <div id="wowslider-container1">
 	<div class="ws_images" style="border:none;"><ul>
-			<li><img src="media/images/planepictureshd.jpg" alt="Accueil" id="wows1_0"/></li>
-			<li><img src="media//images/planepictureshd.jpg" alt="wow slider" id="wows1_1"/></li>
-			<li><img src="media/images/osakaairportnightbuswallpaper.jpg" alt="osaka-airport-night-bus-wallpaper" id="wows1_2"/></li>
+			<li><img src="media/images/parking2.jpg" alt="Accueil" id="wows1_0"/></li>
+			<li><img src="media/images/parking1.jpg" alt="wow slider" id="wows1_1"/></li>
+			<li><img src="media/images/vols.jpg" alt="wow slider2" id="wows1_2"/></li>
+
 		</ul></div>
 	<div class="ws_bullets"><div>
 			<a href="#" title="abstarct_bluetexture"><span>1</span></a>
