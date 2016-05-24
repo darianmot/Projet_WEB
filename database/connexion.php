f638828cdcc8a7b88cd9509a22f35451e7a07003
Anas Darwich <anas.darwich @aliceadsl.fr>
2016-05-24 19:06:23 +0200

    #ajout des variables de session solde prenom nom mail

--- database/connexion.php
+++ database/connexion.php
@@ -7,7 +7,7 @@ session_start();
 $connection = new Connection();
 $bdd = $connection->getBdd();
 
-$reponse = $bdd->query("SELECT id_utilisateur,password  FROM Utilisateur WHERE id_utilisateur='{$_POST['identifiant']}'");
+$reponse = $bdd->query("SELECT id_utilisateur,password,mail,prenom,nom,solde  FROM Utilisateur WHERE id_utilisateur='{$_POST['identifiant']}'");
 $donnees=$reponse->fetch_assoc();
 
 
@@ -17,7 +17,10 @@ if (isset($_POST['password'])AND isset($_POST['identifiant']))
     {   
         $_SESSION['identifiant']=$_POST['identifiant'];
         $_SESSION['password']=$_POST['password'];
-        
+        $_SESSION['nom']=$donnees['nom'];
+        $_SESSION['prenom']=$donnes['prenom'];
+        $_SESSION['mail']=$donnees['mail'];
+        $_SESSION['solde']=$donnees['solde'];
         if ($_SESSION['identifiant']== 'admin')
         {
             echo ('admin');
