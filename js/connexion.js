f1fcd9e871f5f5e7beb95b1aa1b343d6d5386a59
Atime Ronda <atime.ronda@gmail.com>
2016-05-25 14:03:53 +0200

    Ajout d'un loader et d'une redirection après connection petite correction

--- js/connexion.js
+++ js/connexion.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
                         $('#bloc_connexion').remove();
                         $('#connexion').prepend('Vous êtes connecté');
                         $('#connexion').append(' </br> Vous allez être redirigé...');
+                        $('#connexion').loader('show');
                         $('#connexion_menu').remove();
                         $('#inscription_menu').remove();
                         $('#menu_nav').append("<li><a href='deconnexion.php'>Deconnexion</a></li> <li><a href='mon_compte.php'>Votre Compte </a></li><li><a href='gestion.php'>Gestion</a></li>")
