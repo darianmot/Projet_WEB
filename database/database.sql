b68987b2aa1de466e1a9255415c8119c68692d9c
Darian <darian.motamed@gmail.com>
2016-05-28 13:54:03 +0200

    Modification de la base

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS IENAC15_aeroport_C;
 CREATE DATABASE IENAC15_aeroport_C;
 USE IENAC15_aeroport_C;
 
@@ -10,9 +11,9 @@ CREATE TABLE Tarif
 )ENGINE=INNODB;
 
 INSERT INTO `Tarif` (`id_tarif`, `prix`) VALUES
-(1, '1*h'),
-(2, '1*h'),
-(3, '1*h');
+(1, '2*h'),
+(2, '10+1*h'),
+(3, '70');
 
 /*Zone*/
 CREATE TABLE Zone
@@ -35,17 +36,16 @@ CREATE TABLE Utilisateur
   nom CHAR(20),
   prenom CHAR(20),
   mail CHAR(50),
-  solde INT,
   PRIMARY KEY (id_utilisateur)
 )ENGINE=INNODB;
 
-INSERT INTO `Utilisateur` (`id_utilisateur`, `password`, `nom`, `prenom`, `mail`,`solde`) VALUES
-('admin', 'admin', 'admin', 'admin', 'admin',3514);
+INSERT INTO `Utilisateur` (`id_utilisateur`, `password`, `nom`, `prenom`, `mail`) VALUES
+('admin', 'admin', 'admin', 'admin', 'admin');
 
 CREATE TABLE ClientWeb
 (
   id_utilisateur CHAR(20) NOT NULL,
-  Solde INT,
+  solde INT,
   PRIMARY KEY(id_utilisateur),
   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
 )ENGINE=INNODB;
@@ -240,7 +240,6 @@ CREATE TABLE Stationnement
   date_debut DATETIME,
   date_fin DATETIME,
   etat CHAR(20),
-  id_facture CHAR(20),
   PRIMARY KEY (id_stationnement),
   FOREIGN KEY (plaque) REFERENCES Vehicule(plaque),
   FOREIGN KEY (id_place) REFERENCES Place(id_place)
