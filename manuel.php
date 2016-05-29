f9b8ab3b9a38cd076b67da3d33b0f6f988610ee2
Simon <simon.aldouri.ravoux@gmail.com>
2016-05-29 23:13:48 +0200

    Manuel terminé

--- manuel.php
+++ manuel.php
@@ -32,7 +32,7 @@
         </div>
         <img class="manuel_img" src="./media/images/manuel/connexion.jpg">
         <div class="manuel_text">
-            Tous les clients possédant un compte peuvent ensuite se connecter sur cette page avec leur identifiant et leur mot de passe. La connexion leur permet d'accéder à la réservation en ligne d'une place et à la gestion de leur compte client.
+            Tous les clients possédant un compte peuvent ensuite se connecter sur cette page avec leur identifiant et leur mot de passe. La connexion leur permet d'accéder à la réservation en ligne d'une place et à la gestion de leur compte client. L'administrateur se connecte avec l'idntifiant "admin" et le mot de pase "admin".
         </div>
         <img class="manuel_img" src="./media/images/manuel/connecte.jpg">
         <div class="manuel_text">
@@ -59,23 +59,54 @@
             Véhicule
         </div>
         <img class="manuel_img" src="./media/images/manuel/vehicules.jpg">
+        <img class="manuel_img" src="./media/images/manuel/delete_vehicle.jpg">
         <div class="manuel_text">
             L'utilisateur peut enregistrer son ou ses véhicules en choisissant la catégorie correspondant (Voiture, Handicapé ou moto) et en indiquant la plaque d'immatriculation. Il pourra par la suite supprimer les véhicules enregistrés si nécessaire.
+        </div>
+
+        <div class="manuel_subtitle">
+            Véhicule
+        </div>
+        <img class="manuel_img" src="./media/images/manuel/solde.jpg">
+        <div class="manuel_text">
+            L'utilisateur peut rajouter de l'argent sur son compte via l'onglet "Solde" du menu "Votre compte" lorsqu'il est connecté.
         </div>
 
         <div class="manuel_subtitle">
             Réservation
         </div>
         <img class="manuel_img" src="./media/images/manuel/reservation.jpg">
+        <img class="manuel_img" src="./media/images/manuel/reservation2.jpg ">
+        <img class="manuel_img" src="./media/images/manuel/reservation3.jpg ">
         <div class="manuel_text">
-           L'utilisateur sélectionne un de ses véhicules enregistrés pour lequel il veut réserver une place. Il précise ensuite la période de réservation qu'il désire à l'aide des calendriers. Enfin il choisit la zone parking qui l'intéresse et visualise le prix. Pour consulter les différences de tarifs, on peut se rendre au préalable dans l'onglets "Tarifs".
+           L'utilisateur sélectionne un de ses véhicules enregistrés pour lequel il veut réserver une place. Il précise ensuite la période de réservation qu'il désire à l'aide des calendriers. Enfin il choisit la zone parking qui l'intéresse et visualise le prix. Pour consulter les différences de tarifs, on peut se rendre au préalable dans l'onglets "Tarifs". Il peut ensuite visualiser ses réservations dans l'onglet "Mes réservations" du menu "Votre compte".
+        </div>
+
+        <div class="manuel_subtitle">
+            Visualisation de l'état d'occupation du parking
+        </div>
+        <img class="manuel_img" src="./media/images/manuel/plan.jpg">
+        <div class="manuel_text">
+            L'utilisateur peut visualiser l'occupation du parking dans les différentes zones et catégories.
         </div>
 
         <div class="manuel_title">
             Partie gestion administrateur
         </div>
-        <img class="manuel_img" src="./media/images/manuel/plan.jpg">
+
+        <img class="manuel_img" src="./media/images/manuel/simul1_admin.jpg">
         <div class="manuel_text">
+            Dans l'onglet "Plan des parkings", l'administrateur connecté peut simuler l'arrrivée de véhicules avec un générateur aléatoire de plaques d'immatriculation. Il peut également visualiser les places occupées et libres dans les trois zones et dans chaque catégorie.
+        </div>
+
+        <img class="manuel_img" src="./media/images/manuel/simul2_admin.jpg">
+        <div class="manuel_text">
+            Dans l'onglet "Plan des parkings", l'administrateur connecté peut ensuite mettre fin aux stationnements.
+        </div>
+
+        <img class="manuel_img" src="./media/images/manuel/gestion_admin.jpg">
+        <div class="manuel_text">
+            Dans cette onglet "Gestion", l'administrateur peut rajouter ou supprimer des places(si celles-ci sont ni occupées ni réservées) dans les trois zones pour chaque catégorie de véhicules. Il peut également modifier les tarifs en fonction des zones.
         </div>
 
     </article>
