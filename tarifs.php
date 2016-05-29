a721aecc006cbe8cb0e8283534129d47211419c1
Simon <simon.aldouri.ravoux@gmail.com>
2016-05-29 14:11:16 +0200

    Placement graphique tarifs dans container

--- tarifs.php
+++ tarifs.php
@@ -20,39 +20,41 @@
         </table>
     </article>
     <img src="./media/images/porco_rosso.jpg" class="tarifs_img">
+
+    <div class="tarifs_graph" id="div_report" style="height:600px;width:700px; "></div>
+    <script>
+        $(document).ready(function () {
+            var tarif1 = [];
+            var tarif2 = [];
+            var tarif3 = [];
+            for (i = 0; i < 100; i++) {
+                tarif1.push([i, prix(1, i)]);
+                tarif2.push([i, prix(2, i)]);
+                tarif3.push([i, prix(3, i)]);
+                i += 5;
+            }
+            $.jqplot('div_report', [tarif1, tarif2, tarif3],
+                {
+                    title: 'Tarif de chaque zone',
+                    axes: {
+                        yaxis: {min: -10, max: 240, label: '€'},
+                        xaxis: {label: 'heures'}},
+                    legend: {
+                        labels: ['Zone 1'.fontcolor('black'), 'Zone 2'.fontcolor('black'), 'Zone 3'.fontcolor('black')],
+                        renderer: $.jqplot.EnhancedLegendRenderer,
+                        show: true
+                    },
+                    series: [{color: '#5FAB78'}],
+                    highlighter: {
+                        show: true,
+                        sizeAdjust: 7.5
+                    }
+                });
+        });
+    </script>
 </section>
 
-<div class="tarifs_graph" id="div_report" style="height:600px;width:700px; "></div>
-<script>
-    $(document).ready(function () {
-        var tarif1 = [];
-        var tarif2 = [];
-        var tarif3 = [];
-        for (i = 0; i < 100; i++) {
-            tarif1.push([i, prix(1, i)]);
-            tarif2.push([i, prix(2, i)]);
-            tarif3.push([i, prix(3, i)]);
-            i += 5;
-        }
-        $.jqplot('div_report', [tarif1, tarif2, tarif3],
-            {
-                title: 'Tarif de chaque zone',
-                axes: {
-                    yaxis: {min: -10, max: 240, label: '€'},
-                    xaxis: {label: 'heures'}},
-                legend: {
-                    labels: ['Zone 1'.fontcolor('black'), 'Zone 2'.fontcolor('black'), 'Zone 3'.fontcolor('black')],
-                    renderer: $.jqplot.EnhancedLegendRenderer,
-                    show: true
-                },
-                series: [{color: '#5FAB78'}],
-                highlighter: {
-                    show: true,
-                    sizeAdjust: 7.5
-                }
-            });
-    });
-</script>
+
 
 </body>
 </html>
