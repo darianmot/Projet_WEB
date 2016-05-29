817df425b81ac2bf6169cf87d67a021e5032114d
Atime Ronda <atime.ronda@gmail.com>
2016-05-29 18:27:38 +0200

    mes réservations 3

--- database/client_manager.php
+++ database/client_manager.php
@@ -58,22 +58,30 @@ Class ClientManager
     }
 
     /*Obtenir le tableau html des factures d'un véhicule*/
-    public function getBill($plaque){
-        $query = $this->getBdd()->query("SELECT Facture.id_facture, Stationnement.id_stationnement, Stationnement.date_debut, Stationnement.date_fin, Facture.prix 
-                                         FROM Facture 
-                                         INNER JOIN Stationnement 
-                                         ON Facture.id_stationnement = Stationnement.id_stationnement 
-                                         WHERE Stationnement.plaque = '{$plaque}'");
-        echo '<table><tr><td>Facture n°</td><td>Début</td><td>Fin</td><td>Prix</td></tr>';
-        foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $facture){
-            echo '<tr>';
-            echo '<td>'.$facture['id_facture'].'</td>';
-            echo '<td>'.$facture['date_debut'].'</td>';
-            echo '<td>'.$facture['date_fin'].'</td>';
-            echo '<td>'.$facture['prix'].'</td>';
-            echo '</tr>';
+    public function getBill(){
+        include ("database/get_list_vehicule.php");
+        $number_vehicule = count($_SESSION['user_vehicule']);
+        for ($i = 0; $i<$number_vehicule; ++$i){
+            $vehicule = $_SESSION['user_vehicule'][$i];
+            $plaque=$vehicule['plaque'];
+            $type = $vehicule['type_vehicule'];
+            echo "</br>Véhicule: {$plaque} Type: {$type}";
+            $query = $this->getBdd()->query("SELECT Facture.id_facture, Stationnement.id_stationnement, Stationnement.date_debut, Stationnement.date_fin, Facture.prix 
+                                             FROM Facture 
+                                             INNER JOIN Stationnement 
+                                             ON Facture.id_stationnement = Stationnement.id_stationnement 
+                                             WHERE Stationnement.plaque = '{$plaque}'");
+            echo '<table id="factures"><tr><td>Facture n°</td><td>Début</td><td>Fin</td><td>Prix</td></tr>';
+            foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $facture){
+                echo '<tr>';
+                echo '<td>'.$facture['id_facture'].'</td>';
+                echo '<td>'.$facture['date_debut'].'</td>';
+                echo '<td>'.$facture['date_fin'].'</td>';
+                echo '<td>'.$facture['prix'].'</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
         }
-        echo '</table>';
     }
 }
 
@@ -98,7 +106,7 @@ if (isset($_POST['id_form'], $_POST['id']))
             break;
 
         case 'getBill':
-            $client->getBill($_POST['plaque']);
+            echo $client->getBill();
             break;
     }
 }
--- history.php
+++ history.php
@@ -10,54 +10,12 @@
 
 <h1>Historique de vos commandes</h1>
 
-<?php
-include ("database/get_list_vehicule.php");
-$number_vehicule = count($_SESSION['user_vehicule']);
-
-for ($i = 0; $i<$number_vehicule; ++$i){
-    $vehicule = $_SESSION['user_vehicule'][$i];
-    echo "</br>Véhicule: {$vehicule['plaque']} Type: {$vehicule['type_vehicule']}";
-    echo "</br><div class='bill_vehicule container'></div>";
-    
-}
-?>
 <div id='facture_table'></div>
 
 <script>
-//    $('.bill_vehicule').ready(function () {
-//        var plaque = "<?php //echo $vehicule['plaque'] ?>//";
-//        var donnees =$(this).serialize()+'&id_form=getBill' + '&id=' + '<?php //echo $_SESSION['identifiant']?>//' + '&plaque=' + plaque;
-//        $.ajax({
-//            type: "POST",
-//            url: "database/client_manager.php",
-//            dataType: 'JSON',
-//            data: donnees,
-//            async: false,
-//            success: function (retour) {
-//                var number_bill = retour.length;
-//                alert('retour ' + retour + 'longueur' + number_bill );
-//                for(var i=0; i<number_bill; i++) {
-//                    var bill = retour[i];
-//                    var id_facture = bill[0];
-//                    var id_stationnement = bill[1];
-//                    var date_debut = bill[2];
-//                    var date_fin = bill[3];
-//                    var prix = bill[4];
-//                    alert(id_facture+ id_stationnement+ date_debut+ date_fin+ prix);
-//                    $(this).html("</br><div class='bill_of_vehicule row'>" + "Facture: " + id_facture + "Stationnement: " + id_stationnement + "Début: " + date_debut + "Fin: " + date_fin + "Prix: " + prix + "</div>")
-//                }
-//            },
-//
-//            error: function (retour) {
-//                alert('Echec critique');
-//                alert(retour);
-//
-//            }
-//        })
-//    })
     $(document).ready(function () {
         var plaque = "<?php echo $vehicule['plaque'] ?>";
-        var donnees =$(this).serialize()+'&id_form=getBill' + '&id=' + '<?php echo $_SESSION['identifiant']?>' + '&plaque=' + plaque;
+        var donnees =$(this).serialize()+'&id_form=getBill' + '&id=' + '<?php echo $_SESSION['identifiant']?>';
         $.ajax({
             type: "POST",
             url: "database/client_manager.php",
